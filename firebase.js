707a761df2bfb48635f042f914c2402cddd81361
unknown <bennie.ben095@gmail.com>
2023-03-02 17:00:52 -0800

    Basic Authentication setup

---
+++ firebase.js
@@ -0,0 +1,95 @@
+// Core
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js'
+
+// Add Firebase products that you want to use
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut  } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js'
+// import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnHYDfZniOZW7pCJeXWTgs23nhdkp_vcM",
+  authDomain: "gdsc-workshop-84b36.firebaseapp.com",
+  projectId: "gdsc-workshop-84b36",
+  storageBucket: "gdsc-workshop-84b36.appspot.com",
+  messagingSenderId: "411528768939",
+  appId: "1:411528768939:web:9c5085d169e0c7e51636ee"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+
+// Frontend
+function fireError(text){
+    Toastify({
+    text,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+        background: "rgb(220 38 38)",
+    },
+    onClick: function(){} // Callback after click
+    }).showToast(); 
+}
+
+function fireSuccess(text){
+    Toastify({
+    text,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+        background: "rgb(22 163 74)",
+    },
+    onClick: function(){} // Callback after click
+    }).showToast(); 
+}
+
+export const handleSignOut = () => {
+    signOut(auth).then(() => {
+        // Re-draw the page
+    }).catch((error) => {
+        fireError(error);
+    });
+}
+
+export const handleLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            if(user)fireSuccess("You are successfully logged in!");
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            fireError(`${errorCode}: ${errorMessage}`);
+            // ..
+        });
+};
+
+export const handleRegister = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            if(user)fireSuccess("You are successfully signed up!");
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            fireError(`${errorCode}: ${errorMessage}`);
+            // ..
+        });
+};
