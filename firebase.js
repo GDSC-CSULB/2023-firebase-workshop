18e410148c861d032586663c6ff93cf44f5df7ab
unknown <bennie.ben095@gmail.com>
2023-03-04 21:46:26 -0800

    Added comments

--- firebase.js
+++ firebase.js
@@ -6,6 +6,8 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebas
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js'
 
 // Your web app's Firebase configuration
+
+/**** Normally this NEVER should be exposed ... ****/
 const firebaseConfig = {
   apiKey: "AIzaSyCnHYDfZniOZW7pCJeXWTgs23nhdkp_vcM",
   authDomain: "gdsc-workshop-84b36.firebaseapp.com",
@@ -14,6 +16,7 @@ const firebaseConfig = {
   messagingSenderId: "411528768939",
   appId: "1:411528768939:web:9c5085d169e0c7e51636ee"
 };
+/**** Normally this NEVER should be exposed ... ****/
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
